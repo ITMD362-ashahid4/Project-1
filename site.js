404ef12583e57bd096e5c43368c0bd905182aa55
Aisha <ashahid4@hawk.iit.edu>
2021-03-14 19:57:11 -0500

    Create function myEvent() with if statement to check if passwords match

--- site.js
+++ site.js
@@ -1,2 +1,7 @@
 var attempt1 = document.getElementById("password");
 var attempt2 = document.getElementById("again");
+
+function myEvent() {
+  if(attempt1.value != attempt2.value)
+  alert("Passwords do not match. Please try again.");
+}

744fa09b86c9185bd6ba97668ea21cea75c248fd
Aisha <ashahid4@hawk.iit.edu>
2021-04-12 15:36:26 -0500

    Add hasjs in jQuery function

--- site.js
+++ site.js
@@ -1,7 +1,12 @@
-var attempt1 = document.getElementById("password");
-var attempt2 = document.getElementById("again");
+jQuery (function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
 
 function myEvent() {
+  var attempt1 = document.getElementById("password");
+  var attempt2 = document.getElementById("again");
   if(attempt1.value != attempt2.value)
   alert("Passwords do not match. Please try again.");
 }
